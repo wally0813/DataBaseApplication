2cffb1b72bcde7c1574eedd91fb73d0e99239bf1
wally0813 <soyeoni0813@gmail.com>
2019-11-24 21:10:08 +0900

    add transaction

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
-	session_start();
-	include "config.php";
+session_start();
+include "config.php";
 ?>
 <!DOCTYPE html>
 <meta charset="utf-8" />
@@ -32,32 +32,36 @@
 
 <?php
 
-	if (!empty($_POST['user_id'])){
+if (!empty($_POST['user_id'])){
 
-		$id = $_POST['user_id'];
-		$pw = $_POST['user_pw'];
-		$genre = $_POST['genre'];
-		$birth = $_POST['birth_year'] * 10000 + $_POST['birth_month'] * 100 + $_POST['birth_date'];
-		$gender = $_POST['gender'];
-		$email = $_POST['email'];
+	$id = $_POST['user_id'];
+	$pw = $_POST['user_pw'];
+	$genre = $_POST['genre'];
+	$birth = $_POST['birth_year'] * 10000 + $_POST['birth_month'] * 100 + $_POST['birth_date'];
+	$gender = $_POST['gender'];
+	$email = $_POST['email'];
 
-		$query_id = "SELECT user_id FROM user_login WHERE user_id='$id'";
+	$query_id = "SELECT user_id FROM user_login WHERE user_id='$id'";
 
-		$result = $conn->query($query_id);
+	$result = $conn->query($query_id);
 
-		if( $result ){
+	if( $result ){
 
-			$row = $result->fetch_array(MYSQLI_ASSOC);
+		$row = $result->fetch_array(MYSQLI_ASSOC);
 
-			if ( !empty($row['user_id']) ){
+		if ( !empty($row['user_id']) ){
 
 			?>
-				<script>
-					alert("user id is duplicate");
-					history.go(-1);
-				</script>
+			<script>
+				alert("user id is duplicate");
+				history.go(-1);
+			</script>
 			<?php
-			}else{
+		}else{
+
+			try{
+			// transaction start
+				$conn->autocommit(FALSE);
 
 				$query_genre = "SELECT genre_idx FROM movie_genre WHERE genre='$genre'";
 				$result = $conn->query($query_genre);
@@ -69,30 +73,28 @@
 					$info_ins = "INSERT INTO user_info(user_id, genre_idx, birth, gender, email) VALUES ('$id', '$genre_idx', '$birth', '$gender','$email')";
 					$result = $conn->query($info_ins);
 
-					$login_ins = "INSERT INTO user_login(user_id, user_pw) VALUES ('$id', '$pw')";
-					$result2 = $conn->query($login_ins);
-
-					if ( $result && $result2 ){
-
-						?>
-						<script>
-							alert("register success");
-						</script>
-
-						<?php	
-							header("Location: login.php");	
-
-					}else{
-
-						?>
-						<script>
-							alert("database error");
-							history.go(-1);
-						</script>
-
-						<?php
-
+					if ( !$result ) {
+						$result->free();
+						throw new Exception($conn->error);
 					}
+
+					$login_ins = "INSERT INTO user_login(user_id, user_pw) VALUES ('$id', '$pw')";
+					$result = $conn->query($login_ins);
+					if ( !$result ) {
+						$result->free();
+						throw new Exception($conn->error);
+					}
+
+					$conn->commit();
+					$conn->autocommit(TRUE)
+
+					?>
+					<script>
+						alert("register success");
+					</script>
+
+					<?php	
+					header("Location: login.php");	
 
 				}else{
 
@@ -104,154 +106,161 @@
 					<?php
 				}
 
+			}catch( Exception $e ) {
+
+				$conn->rollback(); 
+				$conn->autocommit(TRUE); 
+
 			}
-
-		}else{
-
-			?>
-				<script>alert("database error")</script>
-			<?php
 
 		}
 
 	}else{
-?>
-<body>
-	<div class="row" id="divPosition" style="padding-left:20px; padding-right:20px">
-    <div>
-	<img class="mb-4" style="margin-left: auto; margin-right: auto; display: block;" src="./images/ewha.svg" alt="" width="72" height="72">
-    <h4 class="mb-3" style="text-align:center"><b>회원 정보 입력</b></h4>
-	</br>
-    <form name="register_user" method="post" action="register.php">
-		<div>
-			<label>아이디</label>
-			<input type="text" class="form-control" name="user_id">
-		</div></br>
-		<div>
-			<label>패스워드</label>
-			<input type="password" class="form-control" name="user_pw">
-		</div>
-		</br>
 
-        <div class="mb-3">
-          <div>
-            <label style="padding-right:10px;">좋아하는 장르</label>
-            <select class="custom-select d-block w-100" name="genre" size="1">
-				<option value=""> choose </option>
-				<option value="범죄"> 범죄 </option>
-				<option value="액션"> 액션 </option>
-				<option value="드라마"> 드라마 </option>
-				<option value="로맨스"> 로맨스 </option>
-				<option value="다큐멘터리"> 다큐멘터리 </option>
-				<option value="sf"> SF </option>
-				<option value="스릴러"> 스릴러 </option>
-				<option value="코미디"> 코미디 </option>
-				<option value="어드벤쳐"> 어드벤쳐 </option>
-				<option value="애니메이션"> 애니메이션 </option>
-				<option value="호러"> 호러 </option>
-				<option value="가족"> 가족 </option>
-				<option value="멜로"> 멜로 </option>
-            </select>
-            </br>
-          </div>
-		  <div style="padding-top:20px;">
-            <label style="padding-right:10px;">생년월일</label>
-            <select class="custom-select d-block w-40" name="birth_year" size="1">
-				<option value=""> year </option>
-				<option value="01"> 2001 </option>
-				<option value="00"> 2000 </option>
-				<option value="99"> 1999 </option>
-				<option value="98"> 1998 </option>
-				<option value="97"> 1997 </option>
-				<option value="96"> 1996 </option>
-				<option value="95"> 1995 </option>
-				<option value="94"> 1994 </option>
-				<option value="93"> 1993 </option>
-				<option value="92"> 1992 </option>
-				<option value="91"> 1991 </option>
-				<option value="90"> 1990 </option>
-				<option value="89"> 1989 </option>
-				<option value="88"> 1988 </option>
-				<option value="87"> 1987 </option>
-            </select>
-			<select class="custom-select d-block w-40" name="birth_month" size="1">
-				<option value=""> month </option>
-				<option value="12"> 12 </option>
-				<option value="11"> 11 </option>
-				<option value="10"> 10 </option>
-				<option value="9"> 9 </option>
-				<option value="8"> 8 </option>
-				<option value="7"> 7 </option>
-				<option value="6"> 6 </option>
-				<option value="5"> 5 </option>
-				<option value="4"> 4 </option>
-				<option value="3"> 3 </option>
-				<option value="2"> 2 </option>
-				<option value="1"> 1 </option>
-			</select>
-			<select class="custom-select d-block w-40" name="birth_date" size="1">
-				<option value=""> month </option>
-				<option value="31"> 31 </option>
-				<option value="30"> 30 </option>
-				<option value="29"> 29 </option>
-				<option value="28"> 28 </option>
-				<option value="27"> 27 </option>
-				<option value="26"> 26 </option>
-				<option value="25"> 25 </option>
-				<option value="24"> 24 </option>
-				<option value="23"> 23 </option>
-				<option value="22"> 22 </option>
-				<option value="21"> 21 </option>
-				<option value="20"> 20 </option>
-				<option value="19"> 19 </option>
-				<option value="18"> 18 </option>
-				<option value="17"> 17 </option>
-				<option value="16"> 16 </option>
-				<option value="15"> 15 </option>
-				<option value="14"> 14 </option>
-				<option value="13"> 13 </option>
-				<option value="12"> 12 </option>
-				<option value="11"> 11 </option>
-				<option value="10"> 10 </option>
-				<option value="9"> 9 </option>
-				<option value="8"> 8 </option>
-				<option value="7"> 7 </option>
-				<option value="6"> 6 </option>
-				<option value="5"> 5 </option>
-				<option value="4"> 4 </option>
-				<option value="3"> 3 </option>
-				<option value="2"> 2 </option>
-				<option value="1"> 1 </option>
-			</select>
-            </br>
-          </div>
-		  <div style="padding-top:20px;">
-            <label style="padding-right:10px;">성별</label>
-            <select class="custom-select d-block w-100" name="gender" size="1">
+		?>
+		<script>alert("database error")</script>
+		<?php
+
+	}
+
+}else{
+	?>
+	<body>
+		<div class="row" id="divPosition" style="padding-left:20px; padding-right:20px">
+			<div>
+				<img class="mb-4" style="margin-left: auto; margin-right: auto; display: block;" src="./images/ewha.svg" alt="" width="72" height="72">
+				<h4 class="mb-3" style="text-align:center"><b>회원 정보 입력</b></h4>
+			</br>
+			<form name="register_user" method="post" action="register.php">
+				<div>
+					<label>아이디</label>
+					<input type="text" class="form-control" name="user_id">
+				</div></br>
+				<div>
+					<label>패스워드</label>
+					<input type="password" class="form-control" name="user_pw">
+				</div>
+			</br>
+
+			<div class="mb-3">
+				<div>
+					<label style="padding-right:10px;">좋아하는 장르</label>
+					<select class="custom-select d-block w-100" name="genre" size="1">
+						<option value=""> choose </option>
+						<option value="범죄"> 범죄 </option>
+						<option value="액션"> 액션 </option>
+						<option value="드라마"> 드라마 </option>
+						<option value="로맨스"> 로맨스 </option>
+						<option value="다큐멘터리"> 다큐멘터리 </option>
+						<option value="sf"> SF </option>
+						<option value="스릴러"> 스릴러 </option>
+						<option value="코미디"> 코미디 </option>
+						<option value="어드벤쳐"> 어드벤쳐 </option>
+						<option value="애니메이션"> 애니메이션 </option>
+						<option value="호러"> 호러 </option>
+						<option value="가족"> 가족 </option>
+						<option value="멜로"> 멜로 </option>
+					</select>
+				</br>
+			</div>
+			<div style="padding-top:20px;">
+				<label style="padding-right:10px;">생년월일</label>
+				<select class="custom-select d-block w-40" name="birth_year" size="1">
+					<option value=""> year </option>
+					<option value="01"> 2001 </option>
+					<option value="00"> 2000 </option>
+					<option value="99"> 1999 </option>
+					<option value="98"> 1998 </option>
+					<option value="97"> 1997 </option>
+					<option value="96"> 1996 </option>
+					<option value="95"> 1995 </option>
+					<option value="94"> 1994 </option>
+					<option value="93"> 1993 </option>
+					<option value="92"> 1992 </option>
+					<option value="91"> 1991 </option>
+					<option value="90"> 1990 </option>
+					<option value="89"> 1989 </option>
+					<option value="88"> 1988 </option>
+					<option value="87"> 1987 </option>
+				</select>
+				<select class="custom-select d-block w-40" name="birth_month" size="1">
+					<option value=""> month </option>
+					<option value="12"> 12 </option>
+					<option value="11"> 11 </option>
+					<option value="10"> 10 </option>
+					<option value="9"> 9 </option>
+					<option value="8"> 8 </option>
+					<option value="7"> 7 </option>
+					<option value="6"> 6 </option>
+					<option value="5"> 5 </option>
+					<option value="4"> 4 </option>
+					<option value="3"> 3 </option>
+					<option value="2"> 2 </option>
+					<option value="1"> 1 </option>
+				</select>
+				<select class="custom-select d-block w-40" name="birth_date" size="1">
+					<option value=""> month </option>
+					<option value="31"> 31 </option>
+					<option value="30"> 30 </option>
+					<option value="29"> 29 </option>
+					<option value="28"> 28 </option>
+					<option value="27"> 27 </option>
+					<option value="26"> 26 </option>
+					<option value="25"> 25 </option>
+					<option value="24"> 24 </option>
+					<option value="23"> 23 </option>
+					<option value="22"> 22 </option>
+					<option value="21"> 21 </option>
+					<option value="20"> 20 </option>
+					<option value="19"> 19 </option>
+					<option value="18"> 18 </option>
+					<option value="17"> 17 </option>
+					<option value="16"> 16 </option>
+					<option value="15"> 15 </option>
+					<option value="14"> 14 </option>
+					<option value="13"> 13 </option>
+					<option value="12"> 12 </option>
+					<option value="11"> 11 </option>
+					<option value="10"> 10 </option>
+					<option value="9"> 9 </option>
+					<option value="8"> 8 </option>
+					<option value="7"> 7 </option>
+					<option value="6"> 6 </option>
+					<option value="5"> 5 </option>
+					<option value="4"> 4 </option>
+					<option value="3"> 3 </option>
+					<option value="2"> 2 </option>
+					<option value="1"> 1 </option>
+				</select>
+			</br>
+		</div>
+		<div style="padding-top:20px;">
+			<label style="padding-right:10px;">성별</label>
+			<select class="custom-select d-block w-100" name="gender" size="1">
 				<option value=""> choose </option>
 				<option value="f"> 여성 </option>
 				<option value="m"> 남성 </option>
-            </select>
-            </br>
-          </div>
-        </div>
-		<div class="mb-3" style="padding-top:20px;">
-          <label>이메일 </label>
-          <input type="text" class="form-control" name="email">
-        </div>
+			</select>
 		</br>
-        <button class="btn btn-success btn-lg btn-block" type="submit" value="register">회원가입</button>
-		<button class="btn btn-success btn-lg btn-block" type="reset" value="cancel">다시 작성하기</button>
-		<a class="btn btn-success btn-lg btn-block" href="login.php">로그인 화면으로 돌아가기</a>
-	</form>
 	</div>
-  </div>
+</div>
+<div class="mb-3" style="padding-top:20px;">
+	<label>이메일 </label>
+	<input type="text" class="form-control" name="email">
+</div>
+</br>
+<button class="btn btn-success btn-lg btn-block" type="submit" value="register">회원가입</button>
+<button class="btn btn-success btn-lg btn-block" type="reset" value="cancel">다시 작성하기</button>
+<a class="btn btn-success btn-lg btn-block" href="login.php">로그인 화면으로 돌아가기</a>
+</form>
+</div>
+</div>
 </body>
 </html>
 
 
 <?php
 
-	}
+}
 
 ?>
--- theater.php
+++ theater.php
@@ -38,6 +38,13 @@ include "header.php";
 			display: flex;
 			flex-direction: column;
 		}
+
+		.container {
+			padding-right: 15px;
+			padding-left: 15px;
+			margin-right: auto;
+			margin-left: auto;
+		}
 	</style>
 
 </head>
@@ -65,116 +72,117 @@ include "header.php";
 		?>
 
 		<div class="vc_row element-row row vc_custom_1478727859541">
-  <div class="wpb_column vc_column_container vc_col-sm-12">
-    <div class="wpb_wrapper">
-      <div class="fw-section hb-fw-599b22a654ae9 without-border no-overlay light-style"
-      style="
-      background-image: url(./images/theater.png);
-      min_height:1000px;
-      background-size: contain;
-      background-color: #FFFFFF; 
-      background-repeat: no-repeat;
-      background-position: center center;
-      padding-top: 40px;
-      padding-bottom: 100px;
-      "/>
-      <div class="row fw-content-wrap">
-        <div class="col-12 nbm">
-          <div id="learning-automated-div" style="text-align: center; font-size: 48px;
-          line-height: 52px; padding: 100px 5px 10px 5px;">
+			<div class="wpb_column vc_column_container vc_col-sm-12">
+				<div class="wpb_wrapper">
+					<div class="fw-section hb-fw-599b22a654ae9 without-border no-overlay light-style"
+					style="
+					background-image: url(./images/theater.png);
+					min_height:1000px;
+					background-size: contain;
+					background-color: #FFFFFF; 
+					background-repeat: no-repeat;
+					background-position: center center;
+					padding-top: 40px;
+					padding-bottom: 100px;
+					"/>
+					<div class="row fw-content-wrap">
+						<div class="col-12 nbm">
+							<div id="learning-automated-div" style="text-align: center; font-size: 48px;
+							line-height: 52px; padding: 100px 5px 10px 5px;">
 
-          <span style="color:#FFFFFF;"><b><?php echo $row['theater_name']; ?></b></span></div>
-          <div style="text-align: center; font-size: 18px; line-height: 22px; padding: 0 5px 40px 5px;">
-            <span style="color:#FFFFFF;"><?php echo $row['theater_address']; ?> </span></div>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-		<main role="main">
+							<span style="color:#FFFFFF;"><b><?php echo $row['theater_name']; ?></b></span></div>
+							<div style="text-align: center; font-size: 18px; line-height: 22px; padding: 0 5px 40px 5px;">
+								<span style="color:#FFFFFF;"><?php echo $row['theater_address']; ?> </span></div>
 
-			
-		<?php } ?>
-		<div class="album py-5 bg-light">
-			<h2 style="text-align: center; margin-bottom: 30px;"> 현재 상영중인 영화 </h2>
-			<div class="container">
-				<?php
-
-				$query_info = "SELECT m.filename, m.movie_idx, m.movie_title FROM movie_info as m JOIN screen_info as s ON m.movie_idx=s.movie_idx WHERE theater_idx='$idx'";
-				$result = $conn->query($query_info);
-				$i = 0;
-				while($row = $result->fetch_array(MYSQLI_ASSOC)){
-					if ( $i % 3 == 0 ){
-						?> </br> <?php 
-					}else{
-						?> &nbsp;&nbsp;&nbsp;&nbsp; <?php  
-					}
-
-					$i++;
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<main role="main">
 
 
-					?>
-					<div class="row row-flex">
-						<div class="col-md-4" style="margin-bottom:30px;">
-							<div class="card mb-4 box-shadow">
-								<img class="card-img-top" src=<?php echo './posters/'.$row['filename'];?> width=200; alt="Card image cap">
-								<div class="card-body">
-									<p class="card-text"><b><?php echo $row['movie_title']; ?></b></p>
-									<div class="d-flex justify-content-between align-items-center">
-										<button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='movie.php?idx=<?php echo $row['movie_idx'] ?>'" style="margin-bottom:20px;">자세히</button>
+			<?php } ?>
+			<div class="album py-5 bg-light">
+				<div class="container">
+					<h2 style="text-align: center; margin-bottom: 30px;"> 현재 상영중인 영화 </h2>
+
+					<?php
+
+					$query_info = "SELECT m.filename, m.movie_idx, m.movie_title FROM movie_info as m JOIN screen_info as s ON m.movie_idx=s.movie_idx WHERE theater_idx='$idx'";
+					$result = $conn->query($query_info);
+					$i = 0;
+					while($row = $result->fetch_array(MYSQLI_ASSOC)){
+						if ( $i % 3 == 0 ){
+							?> </br> <?php 
+						}else{
+							?> &nbsp;&nbsp;&nbsp;&nbsp; <?php  
+						}
+
+						$i++;
+
+
+						?>
+						<div class="row row-flex">
+							<div class="col-md-4" style="margin-bottom:30px;">
+								<div class="card mb-4 box-shadow">
+									<img class="card-img-top" src=<?php echo './posters/'.$row['filename'];?> width=200; alt="Card image cap">
+									<div class="card-body">
+										<p class="card-text"><b><?php echo $row['movie_title']; ?></b></p>
+										<div class="d-flex justify-content-between align-items-center">
+											<button type="button" class="btn btn-sm btn-outline-secondary" onclick="location.href='movie.php?idx=<?php echo $row['movie_idx'] ?>'" style="margin-bottom:20px;">자세히</button>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					<?php } ?>
+						<?php } ?>
 
+					</div>
 				</div>
 			</div>
-		</div>
-		<h2 style="text-align: center; margin-bottom: 100px;"> STORE </h2>
-		<div class="table-responsive">
-			<?php
+			<h2 style="text-align: center; margin-bottom: 100px;"> STORE </h2>
+			<div class="table-responsive">
+				<?php
 
-			$query_info = "SELECT * FROM menu_info WHERE theater_idx='$idx'";
+				$query_info = "SELECT * FROM menu_info WHERE theater_idx='$idx'";
 
-			$result = $conn->query($query_info);
+				$result = $conn->query($query_info);
 
-			?>
-			<table class="table table-striped tabl-sm">
-				<thread>
-					<tr>
-						<th>메뉴이름</th>
-						<th>가    격</th>
-						<th>부가설명</th>
-					</tr>
-				</thread>
-
-				<tbody>
-					<?php
-
-					while($row = $result->fetch_array(MYSQLI_ASSOC)){
-
-						?>
-						<tr>
-							<td><?php echo $row['menu_id']; ?> </td>
-							<td><?php echo $row['price']; ?> </td>
-							<td><?php echo $row['description']; ?> </td>
-
-						</tr>
-					</tbody>
-					<?php
-				}
 				?>
-			</table>
+				<table class="table table-striped tabl-sm">
+					<thread>
+						<tr>
+							<th>메뉴이름</th>
+							<th>가    격</th>
+							<th>부가설명</th>
+						</tr>
+					</thread>
+
+					<tbody>
+						<?php
+
+						while($row = $result->fetch_array(MYSQLI_ASSOC)){
+
+							?>
+							<tr>
+								<td><?php echo $row['menu_id']; ?> </td>
+								<td><?php echo $row['price']; ?> </td>
+								<td><?php echo $row['description']; ?> </td>
+
+							</tr>
+						</tbody>
+						<?php
+					}
+					?>
+				</table>
 
 
-		</main>
+			</main>
 
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-		<script src="../../assets/js/vendor/popper.min.js"></script>
-		<script src="../../dist/js/bootstrap.min.js"></script>
-		<script src="../../assets/js/vendor/holder.min.js"></script>
-	</body>
-	</html>
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+			<script src="../../assets/js/vendor/popper.min.js"></script>
+			<script src="../../dist/js/bootstrap.min.js"></script>
+			<script src="../../assets/js/vendor/holder.min.js"></script>
+		</body>
+		</html>
