a5d920ba5b58159bfb9db88cba944f9f548a94af
wally0813 <soyeoni0813@gmail.com>
2019-11-24 17:00:11 +0900

    add header.php

--- index.php
+++ index.php
@@ -1,70 +1,8 @@
 <?php
 
-session_start();
-
-include "config.php";
+include "header.php";
 
 ?>
-<!DOCTYPE html>
-<meta charset="utf-8" />
-<html>
-<head>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Jekyll v3.8.5">
-  <title>Index</title>
-
-  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/carousel/">
-
-  <link rel="stylesheet" href="css/bootstrap.css">
- 
-  <style>
-  .circle{
-   /*margin-left:11.5rem;*/
-  display:inline-block;
-  text-align: center;
-  width: 216px;
-  height: 216px;
-  border-radius: 50%;
-  overflow: hidden;
-  box-shadow: 3px 3px 5px 3px;
- 
-  }
-  .circle img {
-  display:inline;
-  width: auto;
-  height: 100%;
-  object-fit: cover;
-  }
-
-  </style>
-
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-</head>
-<body style="padding-left:30px; padding-right:30px">
-
-<?php
-
-if(!isset($_SESSION['user_id'])){
-?>
-	<a href="./login.php">login</a>
-	<a href="./register.php">register</a>
-
-<?php
-
-	}else{
-
-?>
-  <div style="text-align:center; margin-top:-20px; margin-bottom:-10px; padding-left:150px;">
-  <b style="font-size:25px"> HELLO! <?php echo $_SESSION['user_id'] ?> </b>
-  <div style="float:right; margin-right:-10px">
-		<button type="button" class="btn btn-sm"><a href="./mypage.php" role="button">mypage</a></button>
-		<button type="button" class="btn btn-sm"><a href="./logout.php" role="button">logout</a></button>
-  </div></div>
-  </br>
 
 <div class="vc_row element-row row vc_custom_1478727859541">
 <div class="wpb_column vc_column_container vc_col-sm-12">
@@ -94,7 +32,7 @@ if(!isset($_SESSION['user_id'])){
 <span style="color:#000000;">이화인이 뽑은 금주의 베스트 무비를 지금 확인해보세요!</span></div>
 <div style="text-align: center;">
   <a class="btn best-movie" href="movie.php?idx=<?php echo $row['movie_idx'] ?>"> 영화 보기>> </a> </br> <?php
-  }
+  
 ?>
 </div>
 </div>
--- movie.php
+++ movie.php
@@ -1,8 +1,6 @@
 <?php
 
-	session_start();
-
-	include "config.php";
+	include "header.php";
 
 	if(!isset($_GET['idx'])){
 
